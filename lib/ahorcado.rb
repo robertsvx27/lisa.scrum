cccd8bd4f696e005c464606ca40c59c32c4fbee9
robertsvx27 <robertsvx@gmail.com>
2016-07-08 19:27:17 -0300

    update ahorcado

--- lib/ahorcado.rb
+++ lib/ahorcado.rb
@@ -1,6 +1,10 @@
 class Ahorcado
 	def initialize
 		@palabra = "ELEFANTE"
+		@acertadas = 0
+		@fallidas = 0
+		@maxFallidas = 6
+		@cantidadLetras = 0
 	end
 	def getPalabra
 		@palabra
@@ -11,5 +15,27 @@ class Ahorcado
 		else
 			0
 		end
+	end
+	def getCantidadLetras
+		@palabra.length
+	end
+	def finJuego
+		if getAcertadas==getCantidadLetras
+		    true
+		elsif getFallidas==@maxFallidas
+			false
+		end
+	end
+	def getAcertadas
+		@acertadas
+	end
+	def getFallidas
+		@fallidas
+	end
+	def sumarAcertada
+		@acertadas +=1		
+	end
+	def sumarFallida
+		@fallidas +=1		
 	end
 end
