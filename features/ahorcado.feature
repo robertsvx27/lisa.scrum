6ba8d701a2a9a2e672a9c755501dd61ee2b3142e
Kleer LA <kleer@kleer-sandbox.(none)>
2016-07-08 18:51:12 -0300

    primeras pruebas

---
+++ features/ahorcado.feature
@@ -0,0 +1,6 @@
+Feature: Ahorcado
+
+Scenario: Al inicio con bienvenida
+Given estoy en la pagina de inicio
+Then quiero ver el mensaje "Bienvenido a Lisa.scrum"
+
---
+++ features/step_definitions/my_steps.rb
@@ -0,0 +1,8 @@
+Given(/^estoy en la pagina de inicio$/) do
+  visit '/'
+end
+
+Then(/^quiero ver el mensaje "(.*?)"$/) do |mensaje|
+	  last_response.body.should=~ /#{mensaje}/m
+end
+
